dee63781cd58448b51f94470992855b93a529be8
sackeyjason <planetearthjason@gmail.com>
2024-12-03 23:10:59 +0000

    Clean up Day 3

--- src/3.js
+++ src/3.js
@@ -1,26 +1,17 @@
 import {loader} from './utils.js';
 
 var input = loader('3.txt')
-
-// var muls = input.match(/mul\(\d+,\d+\)/g);
-// sum = muls.map(m => {
-//     var n = m.match(/(\d+),(\d+)/);
-//     return n[1] * n[2];
-// }).reduce((a, b) => a + b);
-// console.log(sum)
-
-var ins = input.match(/mul\(\d+,\d+\)|do\(\)|don't\(\)/g)
+var instructions = input.match(/mul\(\d+,\d+\)|do\(\)|don't\(\)/g)
 var sum = 0;
 var doing = true;
-ins.forEach((x) => {
-    if (x === 'do()') {
+instructions.forEach((instruction) => {
+    if (instruction === 'do()') {
         doing = true;
-    } else if (x === "don't()") {
+    } else if (instruction === "don't()") {
         doing = false;
     } else if (doing) {
-        var [_, a, b] = x.match(/(\d+),(\d+)/);
+        var [_, a, b] = instruction.match(/(\d+),(\d+)/);
         sum = sum + (a * b);
     }
 });
-
-console.log({ins , sum});
+console.log({ sum });
